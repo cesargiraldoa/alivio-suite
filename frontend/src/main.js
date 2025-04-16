b2338305eec679a575659313caaa5233aae0a661
cesargiraldoa <cesargiraldopersonal@gmail.com>
2025-04-15 21:51:04 -0500

    Delete frontend directory

--- frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Login from './components/Login.vue'
-
-const app = createApp(App)
-app.component('Login', Login)
-app.mount('#app')
