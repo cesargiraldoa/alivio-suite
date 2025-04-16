2e432d704c964f402f81a8bcf162038d12d9681a
cesargiraldoa <cesargiraldopersonal@gmail.com>
2025-04-15 20:44:44 -0500

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Bienvenido a Alivio</h1>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style>
+h1 {
+  font-family: Arial, sans-serif;
+  color: #42b983;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
