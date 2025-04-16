60b9701afe2a74ac5db4da976207a0a3804d457a
cesargiraldoa <cesargiraldopersonal@gmail.com>
2025-04-15 20:28:04 -0500

    Delete alivio_frontend_v1 directory

--- alivio_frontend_v1/src/pages/Dashboard.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="p-6">
-    <h1 class="text-2xl font-semibold">Bienvenido a Alivio</h1>
-    <p class="mt-2 text-gray-600">Este será tu panel principal.</p>
-  </div>
-</template>
