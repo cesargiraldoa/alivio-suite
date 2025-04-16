17c84dc54f9859da1c1a74092f120cf8e72aee39
cesargiraldoa <cesargiraldopersonal@gmail.com>
2025-04-15 20:24:45 -0500

    Add files via upload

---
+++ alivio_frontend_v1/src/pages/Dashboard.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-semibold">Bienvenido a Alivio</h1>
+    <p class="mt-2 text-gray-600">Este será tu panel principal.</p>
+  </div>
+</template>
